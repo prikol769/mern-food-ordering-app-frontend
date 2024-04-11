66a81d276aa6342cf06e859e25f45021d59c0e7d
prikol769 <prikol769@gmail.com>
2024-04-11 15:06:00 -0500

    frontend

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,72 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Form, FormControl, FormField, FormItem } from "./ui/form";
+import { Search } from "lucide-react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+
+type Props = {
+  placeHolder: string;
+  searchQuery?: string;
+};
+
+const formSchema = z.object({
+  searchQuery: z.string({
+    required_error: "Restaurant name is required",
+  }),
+});
+
+export type SearchForm = z.infer<typeof formSchema>;
+
+const SearchBar = ({ placeHolder, searchQuery }: Props) => {
+  const form = useForm<SearchForm>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      searchQuery,
+    },
+  });
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={() => {}}
+        className={`flex items-center gap-3 justify-between flex-row border-2 rounded-full p-3 `}
+      >
+        <Search
+          strokeWidth={2.5}
+          size={30}
+          className="ml-1 text-orange-500 hidden md:block"
+        />
+        <FormField
+          name="searchQuery"
+          render={({ field }) => (
+            <FormItem className="flex-1">
+              <FormControl>
+                <Input
+                  {...field}
+                  className="border-none shadow-none text-xl focus-visible:ring-0"
+                  placeholder={placeHolder}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <Button
+          onClick={() => {}}
+          type="button"
+          variant="outline"
+          className="rounded-full"
+        >
+          Reset
+        </Button>
+        <Button type="submit" className="rounded-full bg-orange-500">
+          Search
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default SearchBar;
---
+++ src/layouts/layout.tsx
@@ -0,0 +1,21 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+
+type LayoutProps = {
+  children: React.ReactNode;
+  showHero?: boolean;
+};
+
+const Layout = ({ children, showHero = false }: LayoutProps) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      {showHero && <Hero />}
+      <div className="container mx-auto flex-1 py-10">{children}</div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
