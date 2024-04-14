423ec1327bc179f9b0c13106fc6303bf408d393b
prikol769 <prikol769@gmail.com>
2024-04-13 19:37:17 -0500

    added CuisinesSection to ManageRestaurantForm

--- src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -3,6 +3,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import DetailsSection from "./DetailsSection";
+import CuisinesSection from "./CuisinesSection";
+import { Separator } from "@/components/ui/separator";
 
 type ManageRestaurantFormProps = {
   onSave: (restaurantFormData: FormData) => void;
@@ -62,6 +64,8 @@ const ManageRestaurantForm = ({
         className="space-y-8 bg-gray-50 p-10 rounded-lg"
       >
         <DetailsSection />
+        <Separator />
+        <CuisinesSection />
       </form>
     </Form>
   );
