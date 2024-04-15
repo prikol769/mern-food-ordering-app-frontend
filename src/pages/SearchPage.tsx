637a63dd800de62b0fe4ca487c6916ca45417474
prikol769 <prikol769@gmail.com>
2024-04-15 16:10:55 -0500

    added useSearchRestaurants Api

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,9 +1,22 @@
+import { useSearchRestaurants } from "@/api/RestaurantApi";
 import { useParams } from "react-router-dom";
 
 const SearchPage = () => {
   const { city } = useParams();
+  const { results, isLoading } = useSearchRestaurants(city);
 
-  return <div>SearchPage {city}</div>;
+  return (
+    <div>
+      SearchPage {city}{" "}
+      <span>
+        {results?.data.map((restaurant) => (
+          <span>
+            {restaurant.restaurantName}, {restaurant.city}
+          </span>
+        ))}
+      </span>
+    </div>
+  );
 };
 
 export default SearchPage;
