f034e99202694b9cd4a030481009daf27a808dd8
prikol769 <prikol769@gmail.com>
2024-04-15 15:54:27 -0500

    added handleSearchSubmit to HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,16 @@
 import landingImage from "../assets/landing.png";
 import appDownloadImage from "../assets/appDownload.png";
-import SearchBar from "@/components/SearchBar";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`,
+    });
+  };
   return (
     <div className="flex flex-col gap-12">
       <div className="md:px-32 px-4 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
@@ -10,7 +18,10 @@ const HomePage = () => {
           Tuck into a takeway today
         </h1>
         <span className="text-xl">Food is just a click away!</span>
-        <SearchBar placeHolder="Search by City or Town" />
+        <SearchBar
+          onSubmit={handleSearchSubmit}
+          placeHolder="Search by City or Town"
+        />
       </div>
       <div className="grid md:grid-cols-2 gap-5">
         <img src={landingImage} alt="mobile app" />
