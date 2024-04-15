077f201550bb636125c2a5c4369409de4bbc0728
prikol769 <prikol769@gmail.com>
2024-04-15 10:46:21 -0500

    added updateRestaurant

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,16 +1,23 @@
 import {
   useCreateMyRestaurant,
   useGetMyRestaurant,
+  useUpdateMyRestaurant,
 } from "@/api/MyRestaurantApi";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantPage = () => {
-  const { createRestaurant, isLoading } = useCreateMyRestaurant();
+  const { createRestaurant, isLoading: isCreateLoading } =
+    useCreateMyRestaurant();
   const { restaurant } = useGetMyRestaurant();
+  const { updateRestaurant, isLoading: iseUpdateLoading } =
+    useUpdateMyRestaurant();
+
+  const isEditing = !!restaurant;
+
   return (
     <ManageRestaurantForm
-      onSave={createRestaurant}
-      isLoading={isLoading}
+      onSave={isEditing ? updateRestaurant : createRestaurant}
+      isLoading={isCreateLoading || iseUpdateLoading}
       restaurant={restaurant}
     />
   );
