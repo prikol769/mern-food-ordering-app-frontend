19eae3b8d188fe1d693a6e651aabbd22abe7b167
prikol769 <prikol769@gmail.com>
2024-04-16 16:15:47 -0500

    added SearchResultCard component

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,20 +1,29 @@
 import { useSearchRestaurants } from "@/api/RestaurantApi";
+import SearchResultCard from "@/components/SearchResultCard";
+import SearchResultInfo from "@/components/SearchResultInfo";
 import { useParams } from "react-router-dom";
 
 const SearchPage = () => {
   const { city } = useParams();
   const { results, isLoading } = useSearchRestaurants(city);
 
+  if (isLoading) {
+    <span>Loading...</span>;
+  }
+
+  if (!results?.data || !city) {
+    return <span>No results found</span>;
+  }
+
   return (
-    <div>
-      SearchPage {city}{" "}
-      <span>
-        {results?.data.map((restaurant) => (
-          <span>
-            {restaurant.restaurantName}, {restaurant.city}
-          </span>
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+      <div id="cuisines-list">insert cuisines here</div>
+      <div id="main-content" className="flex flex-col gap-5">
+        <SearchResultInfo total={results.pagination.total} city={city} />
+        {results.data.map((restaurant) => (
+          <SearchResultCard key={restaurant._id} restaurant={restaurant} />
         ))}
-      </span>
+      </div>
     </div>
   );
 };
