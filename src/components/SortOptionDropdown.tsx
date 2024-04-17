e4d28cc596ce0b22d26ae4a4eea1529560036662
prikol769 <prikol769@gmail.com>
2024-04-17 10:27:37 -0500

    added SortOptionDropdown component

---
+++ src/components/SortOptionDropdown.tsx
@@ -0,0 +1,61 @@
+import { buttonVariants } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+
+type Props = {
+  onChange: (value: string) => void;
+  sortOption: string;
+};
+
+const SORT_OPTIONS = [
+  {
+    label: "Best match",
+    value: "bestMatch",
+  },
+  {
+    label: "Delivery price",
+    value: "deliveryPrice",
+  },
+  {
+    label: "Estimated delivery time",
+    value: "estimatedDeliveryTime",
+  },
+];
+
+const SortOptionDropdown = ({ onChange, sortOption }: Props) => {
+  const selectedSortLabel =
+    SORT_OPTIONS.find((option) => option.value === sortOption)?.label ||
+    SORT_OPTIONS[0].label;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <span
+          className={buttonVariants({
+            variant: "outline",
+            className: "w-full",
+          })}
+        >
+          Sort by: {selectedSortLabel}
+        </span>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {SORT_OPTIONS.map((option) => (
+          <DropdownMenuItem
+            key={option.value}
+            className="cursor-pointer"
+            onClick={() => onChange(option.value)}
+          >
+            {option.label}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default SortOptionDropdown;
