85ba7eabd2816b70cf574fd376897312b5396d35
prikol769 <prikol769@gmail.com>
2024-04-24 17:57:33 -0500

    changed Toaster position

--- src/main.tsx
+++ src/main.tsx
@@ -22,7 +22,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <QueryClientProvider client={queryClient}>
         <Auth0ProviderWithNavigate>
           <AppRoutes />
-          <Toaster visibleToasts={1} position="top-right" richColors />
+          <Toaster visibleToasts={1} position="bottom-right" richColors />
         </Auth0ProviderWithNavigate>
       </QueryClientProvider>
     </Router>
