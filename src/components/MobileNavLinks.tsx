77d7f28e679d8a10f0c391660f3953d10633c830
prikol769 <prikol769@gmail.com>
2024-04-25 12:05:37 -0500

    added SheetClose after press link

--- src/components/MobileNavLinks.tsx
+++ src/components/MobileNavLinks.tsx
@@ -1,30 +1,37 @@
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
 import { useAuth0 } from "@auth0/auth0-react";
+import { SheetClose } from "./ui/sheet";
 
 const MobileNavLinks = () => {
   const { logout } = useAuth0();
 
   return (
     <>
-      <Link
-        to="/order-status"
-        className="flex bg-white items-center font-bold hover:text-orange-500"
-      >
-        Order Status
-      </Link>
-      <Link
-        to="/manage-restaurant"
-        className="flex bg-white items-center font-bold hover:text-orange-500"
-      >
-        My Restaurant
-      </Link>
-      <Link
-        to="/user-profile"
-        className="flex bg-white items-center font-bold hover:text-orange-500"
-      >
-        User Profile
-      </Link>
+      <SheetClose asChild key="order-status">
+        <Link
+          to="/order-status"
+          className="flex bg-white items-center font-bold hover:text-orange-500"
+        >
+          Order Status
+        </Link>
+      </SheetClose>
+      <SheetClose asChild key="manage-restaurant">
+        <Link
+          to="/manage-restaurant"
+          className="flex bg-white items-center font-bold hover:text-orange-500"
+        >
+          My Restaurant
+        </Link>
+      </SheetClose>
+      <SheetClose asChild key="user-profile">
+        <Link
+          to="/user-profile"
+          className="flex bg-white items-center font-bold hover:text-orange-500"
+        >
+          User Profile
+        </Link>
+      </SheetClose>
       <Button
         onClick={() => logout()}
         className="flex items-center font-bold px-3 hover:bg-gray-500"
